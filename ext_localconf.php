46ddb447eef3e312d275ca41c5d0ae528c93f957
Benni <benni@typo3.org>
2016-07-28 22:04:19 +0200

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,4 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['connectToDB']['SimpleSSL'] = \CMSExperts\SimpleSSL\Redirector::class . '->redirectCurrentDomainToHttps';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['connectToDB']['SimpleSSL'] = \CMSExperts\Simplessl\Redirector::class . '->redirectCurrentDomainToHttps';
